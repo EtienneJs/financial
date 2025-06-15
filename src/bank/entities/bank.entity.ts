20f3319aac233f09c75c9b58eb4de58af49c89ed
etienne <etiennemfernandez@gmail.com>
2025-06-15 16:54:52 -0400

    Add TypeORM entity for Bank and update dependencies

--- src/bank/entities/bank.entity.ts
+++ src/bank/entities/bank.entity.ts
@@ -1,1 +1,26 @@
-export class Bank {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'banco'})
+export class Bank {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+        nullable: false,
+    })
+    name: string;
+
+    @Column ('text', {
+        nullable: true,
+        array: true,
+        default:[]
+    })
+
+    account: string[];
+
+    @Column('text', {
+        nullable: true
+    })
+    image: string;
+}
