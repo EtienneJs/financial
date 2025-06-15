e561212aaf4951b9f9c95e27f6971d91029d448c
etienne <etiennemfernandez@gmail.com>
2025-06-15 14:23:10 -0400

    Refactor code structure for improved readability and maintainability

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { BankModule } from './bank/bank.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    BankModule
+  , ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ['.env', '.env.local'],
+  }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
